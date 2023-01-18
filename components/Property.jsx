2bda54d0e8bbf6173cb7733161185d0630221b3c
MBANK\munkhtamir.g <munkhtamir.g@m-bank.mn>
2023-01-18 16:38:21 +0800

    MOD

--- components/Property.jsx
+++ components/Property.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
-import { Fabed, FaBath } from "react-icons/fa";
+import { FaBed, FaBath } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
@@ -25,20 +25,48 @@ const Property = ({
     <Flex
       flexWrap="wrap"
       w="420px"
-      paddingTop="0"
+      p="5"
+      paddingTop="0px"
       justifyContent="flex-start"
       cursor="pointer"
     >
       <Box>
         <Image
           src={coverPhoto ? coverPhoto.url : DefaultImage}
-          alt="house"
           width={400}
           height={260}
         />
       </Box>
+      <Box w="full">
+        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED {price}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+          <Box>
+            <Avatar size="sm" src={agency?.logo?.url}></Avatar>
+          </Box>
+        </Flex>
+        <Flex
+          alignItems="center"
+          p="1"
+          justifyContent="space-between"
+          w="250px"
+          color="blue.400"
+        >
+          {rooms}
+          <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+        </Flex>
+        <Text fontSize="lg">
+          {title.length > 30 ? title.substring(0, 30) + "..." : title}
+        </Text>
+      </Box>
     </Flex>
-    {title}
   </Link>
 );
 
